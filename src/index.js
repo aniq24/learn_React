445f848211356e82ee76e9e0709f7eca56f4d77e
aniq24 <aniq24@gmail.com>
2023-09-25 18:16:37 +0800

    stopped at React Array Methods

--- src/index.js
+++ src/index.js
@@ -1,24 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function basicCalculation(x,y){
+let hello = val => 'Hello' + val;
 
-  const add = x + y;
-  const subtract = x - y;
-  const multiply = x * y;
-  const divide = x / y;
-
-  return [add, subtract, multiply, divide];
-}
-
-const [add, subtract, multiply, divide] = basicCalculation(4,2);
-let result = (<div>
-                <p>Sum: {add}</p>
-                <p>Subtract: {subtract}</p>
-                <p>Multiply: {multiply}</p>
-                <p>Divide: {divide}</p>
-              </div>
-);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(result);
+root.render(hello(" World!"));
